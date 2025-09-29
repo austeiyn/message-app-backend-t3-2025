c9dc1abd3c881bde5b62605a0dae972902d0932a
Austine Laman <austinezlaman@gmail.com>
2025-09-29 14:33:16 +1300

    1. Django Installation 2. Database 3. Rest Framework

--- message_app/settings.py
+++ message_app/settings.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,11 +86,11 @@ WSGI_APPLICATION = 'message_app.wsgi.app'
 DATABASES = {
     "default":{
         "ENGINE":"django.db.backends.postgresql",
-        "NAME":"neondb",
-        "USER":"neondb_owner",
-        "PASSWORD":"npg_X0BYWSj6Eflh",
-        "HOST":"ep-lingering-meadow-adm8aqaz-pooler.c-2.us-east-1.aws.neon.tech",
-        "PORT":"5432",
+        "NAME": os.getenv("DB_NAME"),
+        "USER":os.getenv("DB_USER"),
+        "PASSWORD":os.getenv("DB_PASSWORD"),
+        "HOST":os.getenv("DB_HOST"),
+        "PORT":os.getenv("DB_PORT"),
     }
 }
 
